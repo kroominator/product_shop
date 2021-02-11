0fe1daf65e2c70d94b680e2f40df9b2374edd885
gintare kruminaite <gint0217@stud.kea.dk>
2021-02-11 12:33:37 +0200

    help

--- productlist.js
+++ productlist.js
@@ -1,5 +1,4 @@
 // alert("Hello! I am an alert box!!");
-
 // i wanna fetch the data and the data is located here
 // then we get a responce, from that responce we are gonna return the jason
 fetch("https://kea-alt-del.dk/t7/api/products")
@@ -11,16 +10,16 @@ fetch("https://kea-alt-del.dk/t7/api/products")
   .then(function (data) {
     handleProductList(data);
   });
-
 //   the function is gonna receive data, that one is simply gonna loop through some and all the products one at a time and call a function
 function handleProductList(data) {
   //   console.log(data);
   data.forEach(showProduct);
 }
-// this function is gonna receive one item aka product
 
+// this function is gonna receive one item aka product
 function showProduct(product) {
   console.log(product);
+
   //grab the template and were gonna pull out the content
   const template = document.querySelector("#smallProductTemplate").content;
   //clone it, what eleement are we ccopying, make a clone, include all the child nodes
@@ -32,25 +31,25 @@ function showProduct(product) {
   ).textContent = `${product.articletype} | ${product.brandname}`;
   //select the h3 and set the textcontent to be equal to product display name
   copy.querySelector("h3").textContent = product.productdisplayname;
+
+  //sold out or on sale, if its true we are gonna grab the article(the wrapping ellement)
+  //the we are gonna give it a classlist
+  //PROBLEM WITH SOLD OUT HERE
+  //   console.log(solddt);
+  if (product.soldout) {
+    copy.querySelector("article").classList.add("soldOut");
+  }
+  if (product.discount) {
+    copy.querySelector("article").classList.add("onSale");
+    //THIS ONE WORKS, math where at the end / divides
+    copy.querySelector(".discounted p").textContent =
+      Math.floor(product.price - product.price * (product.discount / 100)) +
+      "dkk";
+    copy.querySelector(".discount").textContent = product.discount + "%";
+  }
   //grab the parent
-  const parent = document.querySelector("main");
+  const parent = document.querySelector("#main_list");
   //   we gonna say parent, heres your new child and we are gonna call it copy
   parent.appendChild(copy);
   //append
 }
-
-/* <article class="smallProduct">
-<h2>Nike</h2>
-<img
-  src="https://kea-alt-del.dk/t7/images/webp/640/1163.webp"
-  alt="Sahara Team India Fanwear Round Neck Jersey"
-/>
-<h3>Blue T20 Indian Cricket Jersey</h3>
-<p class="subtle">Tshirts | Nike</p>
-<p class="price"><span>Prev.</span> DKK 1595,-</p>
-<div class="discounted">
-  <p>Now DKK 1560,-</p>
-  <p>-34%</p>
-</div>
-<a href="product.html">Read More</a>
-</article> */
